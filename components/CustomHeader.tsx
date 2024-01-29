216fc6936505d01e9bde477d8cd8c84ac1563371
DNI9 <itsr9boi@gmail.com>
2024-01-30 02:11:11 +0530

    setup nativewind

--- components/CustomHeader.tsx
+++ components/CustomHeader.tsx
@@ -1,80 +1,35 @@
-import { BorderRadius, Colors } from "@/constants/Theme";
+import colors from "@/constants/colors";
 import { Ionicons } from "@expo/vector-icons";
 import React from "react";
-import {
-  Image,
-  ImageStyle,
-  Text,
-  TextStyle,
-  TouchableOpacity,
-  View,
-  ViewStyle,
-} from "react-native";
+import { Image, Text, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function CustomHeader() {
   return (
-    <SafeAreaView style={$safeArea}>
-      <View style={$container}>
-        <Image style={$bike} source={require("@/assets/images/bike.png")} />
+    <SafeAreaView className="flex-1">
+      <View className="h-20 bg-white flex-row items-center justify-between gap-5 px-5">
+        <Image
+          className="h-8 w-8"
+          source={require("@/assets/images/bike.png")}
+        />
 
-        <TouchableOpacity style={$titleContainer}>
-          <Text style={$title}>Deliver - Now</Text>
-          <View style={$locationName}>
-            <Text style={$subtitle}>Select location</Text>
-            <Ionicons name="chevron-down" size={20} color={Colors.primary} />
+        <TouchableOpacity className="flex-1">
+          <Text className="text-gray-400 text-base">Deliver - Now</Text>
+          <View className="flex-row items-center gap-2">
+            <Text className="text-lg font-extrabold">Select location</Text>
+            <Ionicons
+              name="chevron-down"
+              size={20}
+              className="text-primary"
+              color={colors.primary}
+            />
           </View>
         </TouchableOpacity>
 
-        <TouchableOpacity style={$profileBtn}>
-          <Ionicons name="person-outline" size={20} color={Colors.primary} />
+        <TouchableOpacity className="bg-gray-100 p-3 rounded-lg">
+          <Ionicons name="person-outline" size={20} color={colors.primary} />
         </TouchableOpacity>
       </View>
     </SafeAreaView>
   );
 }
-const $safeArea = {
-  flex: 1,
-  backgroundColor: "#fff",
-};
-
-const $container: ViewStyle = {
-  height: 75,
-  backgroundColor: Colors.light,
-  flexDirection: "row",
-  alignItems: "center",
-  justifyContent: "space-between",
-  gap: 20,
-  paddingHorizontal: 20,
-};
-
-const $bike: ImageStyle = {
-  width: 30,
-  height: 30,
-};
-
-const $titleContainer: ViewStyle = {
-  flex: 1,
-};
-
-const $title: TextStyle = {
-  color: Colors.medium,
-  fontSize: 14,
-};
-
-const $subtitle: TextStyle = {
-  fontSize: 18,
-  fontWeight: "bold",
-};
-
-const $locationName: ViewStyle = {
-  flexDirection: "row",
-  gap: 2,
-  alignItems: "center",
-};
-
-const $profileBtn: ViewStyle = {
-  backgroundColor: Colors.lightGrey,
-  padding: 10,
-  borderRadius: BorderRadius.md,
-};
---
+++ constants/colors.ts
@@ -0,0 +1,4 @@
+export default {
+  primary: "#20E1B2",
+  green: "#437919",
+};
